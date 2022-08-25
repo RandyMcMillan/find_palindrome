17ce512a72c87b7c5bf53d1bf546d3900b5ee884
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-25 11:39:45 -0400

    Initial Commit

---
+++ find-palindrome/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  find-palindrome
+//
+//  Created by git on 8/25/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
